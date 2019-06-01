2a5498956936d8393a3b2c716835eec5acb05edd
jaewook0106 <larsson0106@naver.com>
2019-06-01 11:15:49 +0900

    ref

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Statistics } from './Statistics';
 import Stopwatch from './Stopwatch';
 
-export const Header = (props) => {
+export const Header = ({title,players}) => {
   // console.log(props);
-  const {title} = props;
+  // const {title,players} = props;  //Destructing props
   return (
     <header>
-      <Statistics players={props.players}></Statistics>
+      <Statistics players={players}></Statistics>
       <h1>{title}</h1>
       <Stopwatch></Stopwatch>
     </header>
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,52 @@ import React from 'react';
 import Counter from './Counter'
 
 
-export const Player = (props) => {
-  return (
-    <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={()=>props.removePlayer(props.id)}>X</button>
-    </span>
-    <span className="player-name">{props.name}</span>
-    <Counter score={props.score} id={props.id} changeScore={props.changeScore}></Counter>
-  </div>
-  );
-}
+export class Player extends React.PureComponent{
+  render(){
+    console.log(this.props.name,'redered');
+    const {id,removePlayer,changeScore,score,name} = this.props;
+
+    return (
+
+      <div className='player'>
+      <span className='player-name'>
+        <button className='remove-player'
+                onClick={() => removePlayer(id)}>x</button>
+      </span>
+        <span className='player-name'>{name}</span>
+        <Counter score={score} id={id} changeScore={changeScore}/>
+      </div>
+    );
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    // console.log('componentWillReceiveProps', nextProps);
+  }
+ 
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    // console.log('shouldComponentUpdate', nextProps);
+    // 변경될 propss를 비교하여 true or false 리턴
+    return nextProps.score !== this.props.score;
+  }
+} 
+
+
+
+
+
+
+
+
+
+// export const Player = (props) => {
+//   return (
+//     <div className='player'>
+//     <span className='player-name'>
+//       <button className='remove-player'
+//               onClick={() => props.removePlayer(props.id)}>x</button>
+//     </span>
+//       <span className='player-name'>{props.name}</span>
+//       <Counter score={props.score} id={props.id} changeScore={props.changeScore}/>
+//     </div>
+//   );
+// }
